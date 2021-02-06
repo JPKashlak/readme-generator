99c937efbad4a490d395811d5225efb50f762f44
jpkashlak <jpkashlak@gmail.com>
2021-02-06 12:10:08 -0600

    set up inquirer and returned a question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,27 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
+// TODO: Include packages needed for this application
+console.log("Hello!")
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt(
+    [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your projecy?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true
+                }
+                else {
+                    console.log ('Please enter your title!')
+                    return false;
+                }
+            }
+        }
+    ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +30,5 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
+promptUser();
 init();
